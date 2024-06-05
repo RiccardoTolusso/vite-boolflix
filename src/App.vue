f029c1c568403ddcb3178dc428c9dd39a03811e3
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-06-05 13:12:18 +0200

    UPD: installed axios and transformed the create url in an axios.get  call with parameters

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script>
 import {store} from './storage'
+import axios from 'axios'
+
 
 export default{
   name:'MyApp',
@@ -7,11 +9,35 @@ export default{
     return {
       store
     }
+  },
+  methods: {
+    apiCall(){
+      axios.get(`${this.store.apiUrl}/${this.store.category}`, {
+        params:{
+          api_key: this.store.key,
+          query: this.store.query,
+          language: this.store.language,
+          include_adult: this.store.adult,
+          page: this.store.page
+        }
+      })
+      .then((result) => {
+        console.log(result)
+        this.store.currentPage = result.data.page;
+        this.store.results = result.data.results;
+        this.store.total_pages = result.data.total_pages;
+        this.store.total_results = result.data.total_results;
+      })
+    }
+  },
+  created(){
+    // axios.get(this.generateApiUrl())
+    this.apiCall()
   }
 }
 </script>
 <template>
-  <h1>Vue Vite Template</h1>
-  <p>Template di partenza per progetti Vite Vue</p>
+  <!-- HEADER -->
+  <!-- MAIN -->
 </template>
 <!-- <style></style> -->
--- src/storage.js
+++ src/storage.js
@@ -2,21 +2,13 @@ import { reactive } from 'vue';
 
 export const store = reactive({
     // API CALL SETTINGS
-    apiUrl:"https://api.themoviedb.org/3/search/",
-    key: 'movie', // movie / tv / multi
-    query: "",
-    language: 'italian',
-    languages:{
-        italian: {
-            code: 'it-IT',
-            image: null
-        },
-        englis: {
-            code: 'en-US',
-            image: null
-        },
-    },
+    apiUrl:"https://api.themoviedb.org/3/search",  
+    key: "9d0f3fa0c8ae5773b297afa984c0304c",
+    category: 'movie', // movie / tv / multi
+    query: "le colline hanno gli occhi",
+    language: 'en-US',
     page: 1,
+    adult: false,
 
     // API CALL DATA
     currentPage: 0,
@@ -24,5 +16,16 @@ export const store = reactive({
     total_pages: 0,
     total_results: 0,
 
+    // LISTS OF OPTIONS
+    // languages:{
+    //     italian: {
+    //         code: 'it-IT',
+    //         image: null
+    //     },
+    //     english: {
+    //         code: 'en-US',
+    //         image: null
+    //     },
+    // },
 
 })
